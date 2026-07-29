ed17b4d34b5ec7dca528bad617b59e5c63434ddb
Shruti Gupta <119288391+shrutig2211@users.noreply.github.com>
2026-07-29 10:39:34 +0530

    Delete Day 1 directory

--- Day 1/task/task.py
+++
@@ -1,4 +0,0 @@
-print("Hello " + input("Enter your name?\n"))
-city=input("Enter the city you grew up into\n")
-pet_name=input("Enter your pet name\n")
-print("The name of your band is" + " " + city + " " + pet_name)
