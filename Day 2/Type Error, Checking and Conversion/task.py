8ae9097c6388a432a4d828799e9345cbcbaebe75
Shruti Gupta <119288391+shrutig2211@users.noreply.github.com>
2026-07-29 10:39:49 +0530

    Delete Day 2 directory

--- Day 2/Type Error, Checking and Conversion/task.py
+++
@@ -1,8 +0,0 @@
-
-
-name_of_user=input("Enter your name ")
-name_length=len(name_of_user)
-
-print(type(name_of_user)) #string
-print(type(name_length)) #int
-print("Number of letters in your name: " + str(name_length))
